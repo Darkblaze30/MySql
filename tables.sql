f7709bd3ee41b2a3f5f495369311c144cb094e83
Darkblaze30 <juanchiquiza30@gmail.com>
2025-05-23 16:22:15 -0500

    feat: :memo: se quito la documentacion

--- tables.sql
+++ tables.sql
@@ -1,4 +1,3 @@
---se crea categories
 
 CREATE TABLE categories(
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -8,7 +7,6 @@ CREATE TABLE categories(
     active BOOLEAN DEFAULT TRUE
 );
 
---se crea warehouses
 
 CREATE TABLE warehouses(
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -18,7 +16,6 @@ CREATE TABLE warehouses(
     active BOOLEAN DEFAULT TRUE
 );
 
---se crea articles y se relaciona con categories
 CREATE TABLE articles(
     id INT PRIMARY KEY AUTO_INCREMENT,
     code VARCHAR(4) NOT NULL,
@@ -29,7 +26,6 @@ CREATE TABLE articles(
     FOREIGN KEY (category_id) REFERENCES categories(id)
     );
 
---se crea inventories y se relaciona con articles y warehouses
 
 CREATE TABLE inventories(
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -45,7 +41,6 @@ CREATE TABLE inventories(
 
 );
 
---se crea provider y se relaciona con categories
 
 CREATE TABLE provider(
     id INT PRIMARY  KEY AUTO_INCREMENT,
@@ -54,7 +49,6 @@ CREATE TABLE provider(
     email VARCHAR(40) NOT NULL UNIQUE,
     category_id INT,
     registered_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    -- es para vefiricar que el score sea de 0 a 5
     score INT NOT NULL CHECK(score>= 0 AND score <= 5),
     active BOOLEAN,
     FOREIGN KEY(category_id) REFERENCES categories(id)
